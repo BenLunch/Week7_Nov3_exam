46a8943a7df8d8d11517478b56dac84b99220484
Ben Lynch <G00403364@atu.ie>
2025-11-03 09:15:38 +0000

    Initial Commit and creation of management main app, constructors, getters and setters.

---
+++ src/main/java/ie/atu/week7/ManagementApp.java
@@ -0,0 +1,11 @@
+package ie.atu.week7;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ManagementApp {
+    public static void main(String[] args) {
+        
+        }
+    }
+}
